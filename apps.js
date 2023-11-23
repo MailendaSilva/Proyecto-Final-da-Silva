d1d3cb50b2999b6f6b1c219a83673d5f1c36e1f1
Mailen da Silva <mailenfreitas@gmail.com>
2023-11-22 23:48:46 -0300

    actualizaciones

--- apps.js
+++ apps.js
@@ -3,68 +3,68 @@ const productos = document.getElementById("productos");
 const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modal-container");
 
-//array con objetos
-const menu = 
-[
-    {id: 1, combo: "COMBO 1", precio: 2500,  descripcion: "hamburguesa simple con papas y gaseosa"},
-    {id: 2, combo: " COMBO 2", precio: 2900,  descripcion: "hamburguesa doble con papas y gaseosa"},
-    {id: 3, combo: "COMBO 3", precio: 3500, descripcion: "hamburguesa triple con papas y gaseosa"},
-    {id: 4, combo: "COMBO 4", precio: 2400, descripcion: "nuggets con papas y gaseosa"},
-    {id: 5, combo: "COMBO 5", precio: 1900,  descripcion: "menu infantil"},
-]
-
 //arranca vacio o con lo que hay en el local
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
+
 //func asincrona
-const getProductos = async () => {
+const getProducts = async () => {
     const response = await fetch("data.json");
     const data = await response.json();
     console.log(data);
+    
+//recorrer array con forEach
+    data.forEach((product)=> {
+        let content = document.createElement("div");
+        content.className="card"
+        content.innerHTML = `
+        <h1> ${product.combo} </h1>
+        <h2> ${product.precio} </h2>
+        <p> ${product.descripcion} </p>
+        
+        `;
+    //Al id le paso todas las "card"
+    productos.append(content)
+    
+    //agrego boton 
+    let comprar = document.createElement("button")
+    comprar.innerText = "COMPRAR";
+    content.append(comprar)
+    
+    //EVENTOS
+    comprar.addEventListener("click",()=>
+    {
+        const producto = {
+            combo: product.combo,
+            cantidad: 1, 
+            precio: product.precio,
+            descripcion: product.descripcion,
+            precioTotal: product.precio 
+        };
+    
+        // Agregar el producto al carrito
+        carrito.push(producto);
+
+        // Actualizar el local storage
+        saveLocal();
+
+        // Alerta de producto agregado al carrito
+        Swal.fire({
+            icon: 'success',
+            title: 'Ha sido agregado al carrito',
+            showConfirmButton: false,
+            timer: 500
+        });
+
+        console.log(carrito)
+        //pedir que cada agregado vaya al local
+        saveLocal();
+        
+    })
+    });
 };
 
-getProductos();
-//recorrer array con forEach
-menu.forEach((product)=> {
-    //creo en HTML las cards me falta darle estilos
-    let content = document.createElement("div");
-    content.className="card"
-    content.innerHTML = `
-    <h1> ${product.combo} </h1>
-    <h2> ${product.precio} </h2>
-    <p> ${product.descripcion} </p>
-    `;
-//Al id le paso todas las "card"
-productos.append(content)
-
-//agrego boton 
-let comprar = document.createElement("button")
-comprar.innerText = "COMPRAR";
-content.append(comprar)
-
-//EVENTOS
-comprar.addEventListener("click",()=>
-{
-    //pusheo al carrito
-    carrito.push ({
-        combo:product.combo,
-        precio:product.precio,
-        descripcion: product.descripcion
-    })
-
-    // alert agregado
-    Swal.fire({
-        icon: 'success',
-        title: 'Ha sido agregado al carrito',
-        showConfirmButton: false,
-        timer: 500
-      })
-    console.log(carrito)
-    //pedir que cada agregado vaya al local
-    saveLocal();
-    
-})
-});
+getProducts();
 
 //guardar y cambiar a string con json
 const saveLocal =()=>{
--- carrito.js
+++ carrito.js
@@ -9,15 +9,13 @@ const funcionCarrito=()=>{
     modalContainer.style.display="flex";
     //se crea modal
     const modalHeader = document.createElement("div");
-    //me falta darle estilos ok
     modalHeader.className = "modal-header"
-    //le creo su html
     modalHeader.innerHTML = `
-    <h1>Carrito</h1>
+    <h1>TU COMPRA</h1>
     `;
     modalContainer.append(modalHeader);
     const modalButton = document.createElement("button");
-    modalButton.innerText = "X";
+    modalButton.innerText = "CERRAR";
 
     //al hacer click se cierra
     modalButton.addEventListener("click",()=>{
@@ -29,41 +27,114 @@ const funcionCarrito=()=>{
     carrito.forEach((product) =>{
         //mostrar contenido
     let carritoContent = document.createElement("div")
+    carritoContent.className="carritoContent"
     carritoContent.innerHTML = `
     <h1> ${product.combo} </h1> 
     <h2> ${product.precio} $ </h2>
+    <span class= "restar"> - </span>
+    <p>Cantidad: ${product.cantidad} </p>
+    <span class= "sumar"> + </span>
     <p> ${product.descripcion} </p>
-    <span class= "delete-product"> ❌ </span>
     `;
-    modalContainer.append(carritoContent);
 
-    //boton eliminar
-    let eliminar=carritoContent.querySelector(".delete-product");
-    eliminar.addEventListener("click",()=>{
-        eliminarProducto(product.id)
+      modalContainer.append(carritoContent);
+
+
+    //boton restar
+      let restar = carritoContent.querySelector(".restar")
+      restar.addEventListener("click",()=>{
+        if (product.cantidad > 0) {
+          product.cantidad -= 1; 
+          product.precioTotal = product.precio * product.cantidad;
+          funcionCarrito(); 
+      }
+  
     })
-    });
-    
-    // suma con reduce. Empieza en 0 y recorre carrito y le suma
-    const totalCompra = carrito.reduce((acumulador, producto) =>acumulador + producto.precio, 0);
 
+    //boton sumar
+    let sumar = carritoContent.querySelector(".sumar")
+      sumar.addEventListener("click",()=>{
+          product.cantidad += 1; 
+          product.precioTotal = product.precio * product.cantidad;
+           funcionCarrito();
+    })
+   
+    });
+  
+
+    // suma con reduce. Empieza en 0 y recorre carrito y le suma
+    const totalCompra = carrito.reduce((acumulador, producto) =>acumulador + producto.precioTotal, 0);
     const totalCarrito = document.createElement("div")
     totalCarrito.className = "total-content"
     totalCarrito.innerHTML = ` <h3> El total de su compra es: $ ${totalCompra} </h3>
-    <button class="finalizarCompra"> FINALIZAR COMPRA </button> `
+    <h6> 🍔🍟💲Se abona en efectivo al recibir el pedido o por mercado pago 🍔🍟💲 </h6>
+     `
     modalContainer.append(totalCarrito)  
-    
-    //Boton finalizar compra
-    
-}
 
-// Función para eliminar un producto específico
-const eliminarProducto = (id) => {
-    const foundId = carrito.find ((element)=> element.id === id);
-    console.log(foundId);
-    carrito = carrito.filter((carritoId)=>{
-        return carritoId !== foundId;
-    })
-    funcionCarrito();
-};
+    //ELIMINAR CARRITO
+    const vaciarCarrito = document.createElement("button");
+    vaciarCarrito.className = "vaciarCarrito";
+    vaciarCarrito.innerHTML = ` <button> VACIAR CARRITO </button>
+    `
+    modalContainer.append(vaciarCarrito);
+  
+    vaciarCarrito.addEventListener("click", () => {
+      carrito = []; // Vaciar el carrito
+      saveLocal();
+      funcionCarrito(); // Actualizar la vista del carrito estando vacia
+    });
+
+     //FINALIZAR COMPRA
+    const realizarCompra = document.createElement("button")
+    realizarCompra.className="realizarCompra"
+    realizarCompra.innerHTML = ` <button> FINALIZAR COMPRA </button>
+    `
+    modalContainer.append(realizarCompra) 
+    realizarCompra.addEventListener("click",finalizarCompra) 
+  }
+
+
 verCarrito.addEventListener("click", funcionCarrito)
+
+  //BOTON FINALIZAR
+  const finalizarCompra = () => {
+    const form = document.createElement("form");
+    const inputNombre = document.createElement("input");
+    inputNombre.setAttribute("type", "text");
+    inputNombre.setAttribute("placeholder", "Ingrese su nombre");
+  
+    const inputDireccion = document.createElement("input");
+    inputDireccion.setAttribute("type", "text");
+    inputDireccion.setAttribute("placeholder", "Ingrese su dirección");
+  
+    const btnEnviar = document.createElement("button");
+    btnEnviar.textContent = "Enviar";
+  
+    form.appendChild(inputNombre);
+    form.appendChild(inputDireccion);
+    form.appendChild(btnEnviar);
+  
+    modalContainer.appendChild(form);
+  
+    btnEnviar.addEventListener("click", () => {
+      const combo = carrito.map(product => product.combo).join(', ');
+      const totalCompra = carrito.reduce((total, product) => total + (product.precio * product.cantidad), 0);
+      enviarWpp(inputNombre.value, inputDireccion.value, combo, totalCompra);
+    });
+  };
+  
+  //ENVIAR PEDIDO AL WPP
+  const enviarWpp = (nombre, direccion, combo, totalCompra) => {
+    const numeroTelefono = "1168176099";
+    let mensaje = "¡Hola!";
+    if (nombre && direccion) {
+      mensaje += ` Mi nombre es ${nombre}. Tengo listo el pedido para la dirección ${direccion} 🍔🍟
+      Mi pedido es ${combo} 
+      Precio final ${totalCompra}
+      Abono en efectivo al recibir el pedido! 💲 💲 
+      `;
+    }
+    const linkWpp = `https://api.whatsapp.com/send?phone=${numeroTelefono}&text=${encodeURIComponent(mensaje)}`;
+    
+    window.open(linkWpp);
+  };
